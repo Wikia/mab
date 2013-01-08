339a15b8b1594454c04c10269ec8e5a3c2db88a8
loganlinn <logan.linn@gmail.com>
2013-01-08 09:52:20 -0800

    Cleanup whitepsace & indenting as I'm reading

--- src/mab/arm.clj
+++ src/mab/arm.clj
@@ -4,28 +4,25 @@
   []
   (java.util.UUID/randomUUID))
 
-
-(defn create-arm 
+(defn create-arm
   "Create a bandit arm."
   ([count value uuid meta] {:count count
-                       :value value
-                       :uuid uuid
-                       :meta meta})
+                            :value value
+                            :uuid uuid
+                            :meta meta})
   ([count value]      (create-arm count value (create-arm-uuid) nil))
   ([count value uuid] (create-arm count value uuid nil))
   ([]                 (create-arm 0 0 (create-arm-uuid) nil)))
 
-
-(defn initialize-arm-vector 
+(defn initialize-arm-vector
   "Create a vector af initialized arms."
   ([n] (vec (repeatedly n  #(create-arm 0 0))))
-  ([counts values] (vec 
-                       (map #(apply create-arm %)
-                            (partition 2 (interleave counts values)))))
-  ([counts values uuids] (vec 
-                       (map #(apply create-arm %)
-                            (partition 3 (interleave counts values uuids))))))
-
+  ([counts values] (vec
+                     (map #(apply create-arm %)
+                          (partition 2 (interleave counts values)))))
+  ([counts values uuids] (vec
+                           (map #(apply create-arm %)
+                                (partition 3 (interleave counts values uuids))))))
 
 (defn arm-count [arm]
   (get arm :count 0))
@@ -50,7 +47,3 @@
 
 (defn arm-position [arms arm]
   (.indexOf arms arm))
-
-
-
-
