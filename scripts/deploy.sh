f3db73066d28f6bc9210640e8368f11dc2b7c62a
Damon Snyder <drsnyder@gmail.com>
2013-01-09 14:13:33 -0800

    making deploy.sh more multiuser friendly

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-lein2 deploy file:///tmp/mab
+TMP=/tmp/mab.$$
+/bin/rm -rf $TMP
+
+lein2 deploy file://$TMP
 rsync -a -e 'ssh -p 880' /tmp/mab/* ops002.huddler.com:/var/www/jars/
+
+/bin/rm -rf $TMP
