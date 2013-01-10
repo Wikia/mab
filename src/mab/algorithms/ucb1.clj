be901ed77bfdebad59c5e9cc20ba982e674df32a
loganlinn <logan.linn@gmail.com>
2013-01-10 09:24:36 -0800

    whitespace changes

--- src/mab/algorithms/ucb1.clj
+++ src/mab/algorithms/ucb1.clj
@@ -1,19 +1,19 @@
 (ns mab.algorithms.ucb1
   (:use [mab simulator arm]))
 
-(defn untested-arm 
+(defn untested-arm
   [arms]
   (first (take 1 (filter #(= (arm-count %) 0) arms))))
 
-(defn update-curiosity-bonus 
+(defn update-curiosity-bonus
   [arm total-draws]
-  (if (and (> (arm-count arm) 0) 
+  (if (and (> (arm-count arm) 0)
            (> total-draws 0))
-    (let [bonus (/ (Math/sqrt (* 2 (Math/log total-draws))) 
+    (let [bonus (/ (Math/sqrt (* 2 (Math/log total-draws)))
                    (float (arm-count arm)))
           update (+ (float (arm-value arm)) bonus)]
       (update-value arm update))
-    arm)) 
+    arm))
 
 (defn update-curiosity-bonus-all [arms total-draws]
   (vec (map #(update-curiosity-bonus % total-draws) arms)))
@@ -22,8 +22,8 @@
 (defn select-arm [arms]
   (if-let [arm (untested-arm arms)]
     arm
-    (nth arms 
-         (max-value-arm-idx 
+    (nth arms
+         (max-value-arm-idx
            (update-curiosity-bonus arms (total-arm-counts arms))))))
 
 (defn test-algorithm
@@ -33,11 +33,10 @@
         bandit (create-bandit sample-space)
         best-arm (best-arm-index sample-space)]
     (println (format "Best arm is %d" best-arm))
-    (simulation-seq->table 
-      (repeatedly-simulate-seq bandit 
+    (simulation-seq->table
+      (repeatedly-simulate-seq bandit
                                select-arm
                                update-arm
                                arms
-                               horizon 
+                               horizon
                                iterations))))
-
