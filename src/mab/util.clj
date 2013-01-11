38e1a454f205b614132c7ab659f0ab5305f98101
Damon Snyder <damon@huddler-inc.com>
2013-01-11 08:51:52 -0800

    adding append to the csv writer

--- src/mab/util.clj
+++ src/mab/util.clj
@@ -2,10 +2,12 @@
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]))
 
+
 (defn write-to-csv 
   "Write the simulation data to the given file."
-  [file data]
-  (with-open [out-file (io/writer file)] 
-    (csv/write-csv out-file data)))
+  [file data & opts]
+  (let [writer (if opts (apply io/writer file opts) (io/writer file))]
+  (with-open [out-file writer] 
+    (csv/write-csv out-file data))))
   
 
