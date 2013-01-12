2df5370f097bfbd02e45b54a3a888e86c0ec33a0
Damon Snyder <damon@huddler-inc.com>
2013-01-11 22:26:07 -0800

    improving gc for large seq

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://www.huddler.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :jvm-opts ["-Xmx1g" "-server"] 
+  :jvm-opts ["-Xmx2g" "-Xms2g" "-server" "-XX:+UseConcMarkSweepGC"] 
   :repl-options { :init-ns mab.repl-helper }
   :profiles {:dev {:plugins [[lein-midje "2.0.3"]]
                    :dependencies [[lein-midje-lazytest "0.1.0"] 
