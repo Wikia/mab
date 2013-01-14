c1d7d07acbcf8bd9cf4515e569080f3a3e3f335e
Damon Snyder <drsnyder@gmail.com,damon@huddler-inc.com>
2013-01-14 09:27:44 -0800

    Fixing src dir in scripts/deploy.sh.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,6 @@ TMP=/tmp/mab.$$
 /bin/rm -rf $TMP
 
 lein2 deploy file://$TMP
-rsync -a -e 'ssh -p 880' /tmp/mab/* ops002.huddler.com:/var/www/jars/
+rsync -a -e 'ssh -p 880' $TMP ops002.huddler.com:/var/www/jars/
 
 /bin/rm -rf $TMP
